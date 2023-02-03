e6c9cfe3085da05834afb8810cd2d914c341a06f
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-02-03 15:25:29 +0330

    initial ac3

---
+++ ac3.py
@@ -0,0 +1,6 @@
+def ac3(csp) -> bool:
+    pass
+
+
+def revise(csp, i, j) -> bool:
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+from ac3 import ac3
 from csp import CSP
 
 if __name__ == '__main__':
     csp = CSP.from_input()
+
+    ac3(csp)
