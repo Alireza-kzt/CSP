71abf56fe758cfbf504259f1f3d07cc447b73535
Amin <Xporo720@gmail.com>
2023-02-04 02:28:51 -0800

    final test MRV , forwardchecking , ac3

--- backtracking.py
+++ backtracking.py
@@ -1,12 +1,12 @@
 from csp import CSP
 from hall import Hall
+from ac3 import ac3
+
+def LCV(csp, hall:Hall, assignment):
+    return hall.domain
 
 
-def LCV():
-    pass
-
-
-def MVR(csp:CSP, assignment:dict):
+def MRV(csp:CSP, assignment:dict):
     
     neighbors = [ (i,len(csp.halls[i].domain)) for i in range(1,csp.n+1) ]
     neighbors.sort(key=lambda x: x[1])
@@ -18,7 +18,9 @@ def MVR(csp:CSP, assignment:dict):
     return None
         
 
-def forwardChecking(csp:CSP,hall:Hall,hall_index:int,value:int):
+def forwardChecking(csp:CSP,hall_index:int,value:int):
+    hall = csp.halls[hall_index]
+    
     csp = csp.copy()
     for neighbor in hall.constraint:
         csp.halls[neighbor].domain = csp.halls[neighbor].domain - {value}
@@ -30,24 +32,40 @@ def forwardChecking(csp:CSP,hall:Hall,hall_index:int,value:int):
     return csp
 
 
-def conflict(csp, hall, value, assignment):
-    for hall_index in hall.constraint:
-        if hall_index in assignment:
-            if assignment[hall_index] == value:
+def conflict(csp:CSP, hall_index:int, value, assignment):
+    hall = csp.halls[hall_index]
+    
+    for h in hall.constraint:
+        if h in assignment:
+            if assignment[h] == value:
                 return True
+            
+    for i in range(1,csp.n+1):
+        if hall_index in csp.halls[i].constraint and i in assignment:
+            if assignment[i] == value:
+                return True
+            
     return False
 
 
-def backtracking(csp:CSP, assignment:dict=dict()) -> dict():
+def backtracking(csp:CSP, assignment:dict=dict() , AI_ac3 = False) -> dict():
     if len(assignment) == csp.n: return assignment
 
-    hall_index = MVR(csp, assignment)
+    hall_index = MRV(csp, assignment)
     hall = csp.halls[hall_index]
 
-    for value in LCV(csp, hall.domain, assignment):
-        if not conflict(csp, hall, value, assignment):
+    for value in LCV(csp, hall, assignment):
+        if not conflict(csp, hall_index, value, assignment):
             assignment[hall_index] = value
-            result = backtracking(forwardChecking(csp,hall,hall_index,value), assignment)
+            
+            if AI_ac3 :
+                _csp = csp.copy()
+                if not ac3(_csp): return False
+            else:
+                _csp = forwardChecking(csp,hall_index,value)
+                
+            result = backtracking(_csp, assignment)
+            
             if result is False :
                 assignment.pop(hall_index)
             else:
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from ac3 import ac3
 from csp import CSP
-from backtracking import forwardChecking,MVR
+from backtracking import backtracking
+from datetime import datetime
 
 if __name__ == '__main__':
     csp = CSP.from_input()
+    
+    start = datetime.now()
+    print(backtracking(csp.copy()))
+    end = datetime.now()
+    print(end-start)
+    start = datetime.now()
+    print(backtracking(csp.copy(),AI_ac3=True))
+    end = datetime.now()
+    print(end-start)
+    
